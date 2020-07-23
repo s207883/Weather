53de90418b0ad3e396257ffc4207f1cbc63c759d
Alexandr G <s207883@yandex.ru>
2020-07-24 00:05:24 +0300

    Реализация контроллера прогноза погоды.

--- Weather.WebAPI/Controllers/WeatherForecastController.cs
+++ Weather.WebAPI/Controllers/WeatherForecastController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Weather.Core.Interfaces;
+using Weather.Core.Models;
+using Weather.Core.RequestModels;
+using Weather.Core.Structs;
+using Weather.Core.ViewModels;
 
 namespace Weather.WebAPI.Controllers
 {
@@ -11,29 +16,72 @@ namespace Weather.WebAPI.Controllers
 	[Route("[controller]")]
 	public class WeatherForecastController : ControllerBase
 	{
-		private static readonly string[] Summaries = new[]
-		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-		};
 
 		private readonly ILogger<WeatherForecastController> _logger;
+		private readonly IWeatherService _weatherService;
 
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		public WeatherForecastController(IWeatherService weatherService, ILogger<WeatherForecastController> logger)
 		{
-			_logger = logger;
+			_weatherService = weatherService ?? throw new ArgumentNullException(nameof(weatherService));
+			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
 		}
 
-		[HttpGet]
-		public IEnumerable<WeatherForecast> Get()
+		/// <summary>
+		/// Получить погоду по координатам.
+		/// </summary>
+		/// <param name="weatherRequestModel">Модель запроса.</param>
+		/// <returns>Прогноз погоды</returns>
+		[HttpGet("[action]")]
+		public async Task<ActionResult<BaseApiResponse<WeatherViewModel>>> GetWeatherByCoordinates([FromQuery] WeatherRequestModelBase weatherRequestModel)
 		{
-			var rng = new Random();
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			if (!ModelState.IsValid)
 			{
-				Date = DateTime.Now.AddDays(index),
-				TemperatureC = rng.Next(-20, 55),
-				Summary = Summaries[rng.Next(Summaries.Length)]
-			})
-			.ToArray();
+				return BadRequest(weatherRequestModel);
+			}
+
+			var weather = await _weatherService.GetWeatherByCoordinatesAsync(new Coordinates(weatherRequestModel.Latitude, weatherRequestModel.Longitude));
+
+			if (weather == default)
+			{
+				var notFoundResponse = new BaseApiResponse { IsSuccess = false, Message = "Данные о погоде не найдены." };
+				return NotFound(notFoundResponse);
+			}
+
+			var response = new BaseApiResponse<WeatherViewModel> { IsSuccess = true, Data = weather };
+
+			return Ok(response);
+		}
+
+		/// <summary>
+		/// Получить погоду по координатам и временному промежутку.
+		/// </summary>
+		/// <param name="weatherRequestModel">Модель запроса.</param>
+		/// <returns>Прогноз погоды.</returns>
+		[HttpGet("[action]")]
+		public async Task<ActionResult<BaseApiResponse<WeatherViewModel>>> GetWeatherByCoordinatesAndDate([FromQuery] WeatherRequestModelWithDate weatherRequestModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(weatherRequestModel);
+			}
+
+			if (DateTime.Compare(weatherRequestModel.StartDate, weatherRequestModel.EndDate) > 0)
+			{
+				var badResponseBody = new BaseApiResponse { IsSuccess = false, Message = "Некорректная дата." };
+				return BadRequest(badResponseBody);
+			}
+
+			var weather = await _weatherService.GetWeatherByCoordinatesAsync(new Coordinates(weatherRequestModel.Latitude, weatherRequestModel.Longitude), new TimeRange(weatherRequestModel.StartDate, weatherRequestModel.EndDate));
+
+			if (weather == default)
+			{
+				var notFoundResponse = new BaseApiResponse { IsSuccess = false, Message = "Данные о погоде не найдены." };
+				return NotFound(notFoundResponse);
+			}
+
+			var response = new BaseApiResponse<WeatherViewModel> { IsSuccess = true, Data = weather };
+
+			return Ok(response);
 		}
 	}
 }
