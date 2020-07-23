e5012288a1c156f6fde77b5a588139f73bb77ca2
Alexandr G <s207883@yandex.ru>
2020-07-24 00:05:02 +0300

    Добавлены модели API

---
+++ Weather.Core/Models/BaseApiResponse.cs
@@ -0,0 +1,30 @@
+﻿namespace Weather.Core.Models
+{
+	/// <summary>
+	/// Базовый ответ API.
+	/// </summary>
+	/// <typeparam name="T">Модель возвращаемых данных.</typeparam>
+	public class BaseApiResponse<T> : BaseApiResponse where T : class
+	{
+		/// <summary>
+		/// Данные.
+		/// </summary>
+		public T Data { get; set; }
+	}
+
+	/// <summary>
+	/// Базовый ответ API.
+	/// </summary>
+	public class BaseApiResponse
+	{
+		/// <summary>
+		/// Успешность запроса.
+		/// </summary>
+		public bool IsSuccess { get; set; }
+
+		/// <summary>
+		/// Сообщение.
+		/// </summary>
+		public string Message { get; set; }
+	}
+}
---
+++ Weather.Core/RequestModels/WeatherRequestModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Weather.Core.RequestModels
+{
+	/// <summary>
+	/// Модель запроса данных о погоде.
+	/// </summary>
+	public class WeatherRequestModelBase
+	{
+		/// <summary>
+		/// Широта.
+		/// </summary>
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Не указана широта")]
+		[Range(1, 360, ErrorMessage = "Недопустимая широта")]
+		public float Latitude { get; set; }
+
+		/// <summary>
+		/// Долгота.
+		/// </summary>
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Не указана долгота")]
+		[Range(1, 360, ErrorMessage = "Недопустимая долгота")]
+		public float Longitude { get; set; }
+	}
+
+	public class WeatherRequestModelWithDate : WeatherRequestModelBase
+	{
+		/// <summary>
+		/// Начальная дата.
+		/// </summary>
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Начальная дата должна быть заполнена.")]
+		public DateTime StartDate { get; set; }
+
+		/// <summary>
+		/// Конечная дата.
+		/// </summary>
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Конечная дата должна быть заполнена.")]
+		public DateTime EndDate { get; set; }
+	}
+}
