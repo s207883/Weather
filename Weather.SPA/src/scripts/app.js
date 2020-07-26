51fb4018532ffa4063f468d3fa91709aa84623bb
Горелкин Александр <s207883@yandex.ru>
2020-07-26 11:28:28 +0300

    Верстка и сборка приложения

--- Weather.SPA/src/scripts/app.js
+++ Weather.SPA/src/scripts/app.js
@@ -1,3 +1,75 @@
 import '../styles/style.css';
 
-import "./chartService";
+import { drawChart, dataColors, borderColors } from "./chartService";
+
+import { getWeatherByCoordinatesAndDate } from './apiRequestService';
+
+const weatherRequestForm = document.getElementById('weather_form');
+
+let chart = undefined;
+
+weatherRequestForm.onsubmit = (async (event) => {
+    event.preventDefault();
+    var latitude = document.getElementById('latitude').value;
+    var longitude = document.getElementById('longitude').value;
+    var dateFrom = document.getElementById('dateFrom').valueAsDate.toLocaleDateString("en-US");
+    var dateTo = document.getElementById('dateTo').valueAsDate.toLocaleDateString("en-US");
+
+    if (latitude != "" & longitude != "" & dateFrom != "" & dateTo != "") {
+        var result = await getWeatherByCoordinatesAndDate({ latitude, longitude, dateFrom, dateTo })
+        if (result.isSuccess) {
+            const weatherData = result.data.weatherData;
+            const weatherTimeRange = result.data.timeRange;
+
+            const dateFromInput = document.getElementById('dateFrom');
+            const dateToInput = document.getElementById('dateTo');
+            dateFromInput.valueAsDate = new Date(weatherTimeRange.startDate);
+            dateToInput.valueAsDate = new Date(weatherTimeRange.endDate);
+
+            let chartData = {
+                labels: [],
+                datasets: [{
+                    label: 'Temperature',
+                    data: [],
+                    backgroundColor: dataColors[0],
+                    borderColor: borderColors[0],
+                    borderWidth: 1,
+                },
+                {
+                    label: 'Humidity %',
+                    data: [],
+                    backgroundColor: dataColors[1],
+                    borderColor: borderColors[1],
+                    borderWidth: 1,
+                },
+                {
+                    label: 'Precipitation in mm',
+                    data: [],
+                    backgroundColor: dataColors[2],
+                    borderColor: borderColors[2],
+                    borderWidth: 1,
+                }],
+            };
+
+            for (const item of weatherData) {
+                chartData.labels.push(new Date(item.weatherDate).toDateString());
+                chartData.datasets[0].data.push(item.temperature);
+                chartData.datasets[1].data.push(item.humidity);
+                chartData.datasets[2].data.push(item.precipitation);
+            }
+
+            const chartElement = document.getElementById('weatherChart');
+
+            if (chart != null && typeof (chart.destroy) === 'function') {
+                chart.destroy();
+            }
+            chart = drawChart(chartElement, chartData);
+        }
+    }
+});
+
+weatherRequestForm.onreset = (async (_) => {
+    if (chart != null && typeof (chart.destroy) === 'function') {
+        chart.destroy();
+    }
+})
