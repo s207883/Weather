f64541ff0d39b9249dee24d6d7e519360632314f
Горелкин Александр <s207883@yandex.ru>
2020-07-28 22:58:34 +0300

    bugFix

--- Weather.SPA/src/scripts/apiRequestService.js
+++ Weather.SPA/src/scripts/apiRequestService.js
@@ -10,7 +10,7 @@ import { apiRoutes, apiHost, apiPort } from "./apiConsts";
  * }} requestData Данные запроса.
  */
 export async function getWeatherByCoordinatesAndDate(requestData) {
-    const route = `${apiRoutes.getHistory}?StartDate=${requestData.dateFrom}&EndDate=${requestData.dateTo}&Latitude=60&Longitude=30`;
+    const route = `${apiRoutes.getHistory}?StartDate=${requestData.dateFrom}&EndDate=${requestData.dateTo}&Latitude=${requestData.latitude}&Longitude=${requestData.longitude}`;
 
     const options = {
         method: 'GET'
